50a2166408de912b442b37db6744e2a1e31fa629
khunkruau <137683680+khunkruau@users.noreply.github.com>
2023-07-21 11:24:42 +0700

    update 21-07-2566

---
+++ re.php
@@ -0,0 +1,43 @@
+﻿<html>
+<head>
+	<title>รับข้อมูลฟอร์ม เงื่อนไข ควบคุมการแสดงผล</title>
+</head>
+<body>
+<h4>#หน้ารับข้อมูลฟอร์ม เงื่อนไข ควบคุมการแสดงผล</h4>
+<?php
+	$animal = $_POST["selanimal"];
+	$rsex = $_POST["sex"];
+	$rfirstname = $_POST["firstname"];
+	$rlastname = $_POST["lastname"];
+?>
+<center>
+	<table width="50%" border="1" cellspacing="0">
+		<tr>
+			<td width="25%" align="center">
+					<?php 
+						if ($animal=="cow"){
+					    	echo "<img src='cow.png' width='45%'>";
+						}else {
+							echo "<img src='deer.png' width='45%'>";
+						}
+					?>
+			</td><td width="25%" align="center"><?php echo "$rsex$rfirstname $rlastname";?></td>
+		</tr>
+		
+		<tr>
+			</td><td width="25%" align="center"><?php echo "$rsex$rfirstname $rlastname";?></td>
+			<td width="25%" align="center">
+					<?php 
+						if ($animal=="cow"){
+					    	echo "<img src='cow.png' width='45%'>";
+						}else {
+							echo "<img src='deer.png' width='45%'>";
+						}
+					?>
+			
+		</tr>
+
+	</table>
+</center>
+</body>
+</html>
